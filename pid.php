7cf1ae1b7e3a294f48e405c86665cf44fe4f6e2b
Nikita Pishvanov <78811277+pishvanov98@users.noreply.github.com>
2023-04-07 12:48:12 +0300

    destroy-procces
    Если процессов 3 и больше убиваем их

---
+++ pid.php
@@ -0,0 +1,43 @@
+<?
+exec("tasklist", $task_list);
+
+
+$i=0;
+$pid_kill_proc_mass=array();
+$coun_proces=0;
+foreach($task_list as $key=>$item){
+	if($i < 4){
+		$i++;
+		continue;
+	}
+$i++;
+
+$massiv= explode(' ',$item);
+$massiv= array_filter($massiv);
+
+if($massiv[0]=="Имя процесса"){
+	$name_proces = array_shift($massiv);
+	$pid_proces = array_shift($massiv);
+	if($coun_proces >= 3){
+		$pid_kill_proc_mass[]= $pid_proces;
+	}
+	$coun_proces++;
+}
+
+}
+
+
+if(!empty($pid_kill_proc_mass)){
+	
+	var_dump($pid_kill_proc_mass);
+	
+	foreach($pid_kill_proc_mass as $item_del){
+		
+		exec("taskkill /PID ".$item_del. " /f");
+		
+	}
+	
+	
+}
+
+?>
